7e3af997186643a4a95476e4ad1ad1355d8df7d3
Ricardo <ricardo.machado.nwi@gmail.com>
2023-08-26 10:08:42 -0300

    Styling

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
 
 export default function App() {
+  console.log('DBG: RUNNING THE APP');
+  console.warn('DBG THE APP');
+
+  const renderText = (times: number) => {
+    const arr = new Array(times).fill('');
+
+    return arr.map((_, index) => <Text>-${index + 1}</Text>);
+  };
+
   return (
+    // <SafeAreaView>
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Look what I did!!!</Text>
+      <ScrollView style={styles.scrollableContainer}>
+        <Text>List:</Text>
+        {renderText(150)}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
+    // </SafeAreaView>
   );
 }
 
@@ -16,5 +32,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 30,
+  },
+
+  scrollableContainer: {
+    width: '80%',
+    border: '1px solid red',
   },
 });
